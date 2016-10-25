f24743c677c5c0fcb2201c168b75124543470432
(s) Christopher Smith <cwsmith@pm7-0064.uopnet.plymouth.ac.uk>
2016-10-25 15:57:36 +0100

    Got scoring working, score prints out when won but may need to improve this feature

--- Visualisation/Visualisation.py
+++ Visualisation/Visualisation.py
@@ -2,6 +2,7 @@ from graphics import *
 import math
 import time
 import random
+from threading import Thread
 
 Window = GraphWin("DAT505 Visualisation", 1080, 720)
 
@@ -13,9 +14,13 @@ CurrentShapes = [0]*len(Scores)
 
 CurrentShapesDone = ["false"]*len(Scores)
 
-CurrentShapesCenter = [25]*len(Scores)
+CurrentShapesCenterX = [0]*len(Scores)
+
+CurrentShapesCenterY = [0]*len(Scores)
 
 Current = 0
+
+GameScore = 0
 
 def createShape(Num):
     NumBlocks = int(math.ceil(int(Scores[Num])/10))
@@ -28,6 +33,8 @@ def createShape(Num):
     for i in range(0, NumBlocks):
         if(Shape[0] == "EMPTY"):
             Shape[0] = Rectangle(Point(StartPos[0],  StartPos[1]), Point(StartPos[0] + 50, StartPos[1] + 50))
+            CurrentShapesCenterX[Num] = Shape[0].getCenter().getX()
+            CurrentShapesCenterY[Num] = Shape[0].getCenter().getY()
             #Shape[i].setFill("yellow")
             #Shape[i].draw(Window)
         else:
@@ -35,12 +42,15 @@ def createShape(Num):
             if(Range == 3):
                 Shape.append(Rectangle(Point(((StartPos[0] + (i*50))-50)-(NumOffset*50), StartPos[1]-50), Point(((StartPos[0] + (i*50)))-(NumOffset*50), StartPos[1])))
                 NumOffset = NumOffset + 1
+                CurrentShapesCenterY[Num] = CurrentShapesCenterY[Num] - 25
             elif(Range == 4):
                 Shape.append(Rectangle(Point(((StartPos[0] + (i*50))-50)-(NumOffset*50), StartPos[1]+50), Point(((StartPos[0] + (i*50)))-(NumOffset*50), StartPos[1] + 100)))
                 NumOffset = NumOffset + 1
+                CurrentShapesCenterY[Num] = CurrentShapesCenterY[Num] + 25
             else:
                 Shape.append(Rectangle(Point((StartPos[0] + (i*50))-(NumOffset*50), StartPos[1]), Point(((StartPos[0] + (i*50))+50)-(NumOffset*50), StartPos[1] + 50)))
-                CurrentShapesCenter[Num] = CurrentShapesCenter[Num] + 25
+                CurrentShapesCenterX[Num] = CurrentShapesCenterX[Num] + 25
+        print("Center X = " + str(CurrentShapesCenterX[Num]) + " and Center Y = " + str(CurrentShapesCenterY[Num]))
         Shape[i].setFill(ShapeColor)
         Shape[i].draw(Window)
     CurrentShapes[Num] = Shape
@@ -50,23 +60,35 @@ createShape(Current)
 
 Playing = True
 
+def dropperThread():
+    while Playing == True:
+        break
+
+thread = Thread(target=dropperThread)
+thread.start()
+
 while Playing == True:
     try:
-        print("Start Loop " + str(Current))
+        SleepTime = 0.4
+        CurrentShapesCenterY[Current] = int(CurrentShapesCenterY[Current]) + 25
         for i in range(0,len(CurrentShapes[Current])):
             CurrentShapes[Current][i].move(0, 25)
-        keyPressed = Window.getKey()
+        
+        keyPressed = Window.checkKey()
         if(keyPressed == "Escape"):
             Playing = False
             print(Playing)
             break
         if(keyPressed == "Left"):
+            CurrentShapesCenterX[Current] = int(CurrentShapesCenterX[Current]) - 50
             for i in range(0,len(CurrentShapes[Current])):
                 if((CurrentShapes[Current][i].getCenter().getX() - 25) <= 0):
                     break
                 else:
                     CurrentShapes[Current][i].move(-50, 0)
+                    print("Moving left center = " + str(CurrentShapesCenterX[Current]))
         elif(keyPressed == "Right"):
+            CurrentShapesCenterX[Current] = int(CurrentShapesCenterX[Current]) + 50
             for i in range(0,len(CurrentShapes[Current])):
                 if((CurrentShapes[Current][i].getCenter().getX() + 25) >= Window.width):
                     break
@@ -74,14 +96,18 @@ while Playing == True:
                     CurrentShapes[Current][i].move(50, 0)
         elif(keyPressed == "Up" or keyPressed == "r"):
             print("ROTATE")
-        
-        print("Mid Loop " + str(Current))
+        elif(keyPressed == "Down"):
+            SleepTime = 0.1
+
         for i in range(0,len(CurrentShapes[Current])):
-            #print("Current is " + str(Current))
-            #print(CurrentShapes[Current][i])
-            #print("PLEASE WORK")
-            if((CurrentShapes[Current][i].getCenter().getY() + 27) >= Window.getHeight()):# or Window.getPixel(CurrentShapes[Current][i].getCenter().getY() + 26, CurrentShapes[Current][i].getCenter().getX()) != "black"):
+            if((CurrentShapes[Current][i].getCenter().getY() + 27) >= Window.getHeight()):
                 print("Reached Bottom")
+                GameScore = GameScore + int(Scores[Current])
+                print(str(GameScore))
+                ScoreText = Text(Point(CurrentShapesCenterX[Current],CurrentShapesCenterY[Current]) , str(Scores[Current]))
+                ScoreText.setSize(35)
+                ScoreText.setFill("white")
+                ScoreText.draw(Window)
                 Current = Current + 1
                 CurrentShapesDone[Current] = "true"
                 createShape(Current)
@@ -99,6 +125,11 @@ while Playing == True:
                         # Window.plot(CurrentShapes[e][o].getCenter().getX(),CurrentShapes[e][o].getCenter().getY() - 27, "red")
                         if((CurrentShapes[Current][i].getCenter().getY() + 27) >= (CurrentShapes[e][o].getCenter().getY() - 27) and (CurrentShapes[Current][i].getCenter().getY() - 27) <= (CurrentShapes[e][o].getCenter().getY() + 27)  and (CurrentShapes[Current][i].getCenter().getX()) == (CurrentShapes[e][o].getCenter().getX())):
                             print("Same")
+                            GameScore = GameScore + int(Scores[Current])
+                            ScoreText = Text(Point(CurrentShapesCenterX[Current],CurrentShapesCenterY[Current]) , str(Scores[Current]))
+                            ScoreText.setSize(35)
+                            ScoreText.setFill("white")
+                            ScoreText.draw(Window)
                             Current = Current + 1
                             CurrentShapesDone[Current] = "true"
                             if(Current >= len(Scores)):
@@ -110,6 +141,7 @@ while Playing == True:
                                 break
     except:
         print("Something is broken, trying again")
+    time.sleep(SleepTime)
 #print("   ")
 
 
